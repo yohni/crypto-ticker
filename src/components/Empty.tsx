37888db89cff565a70510338c73ad29b840c5173
yohni <yohni.123@gmail.com>
2022-01-25 11:09:15 +0700

    feat: 🎸 create empty page

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { MdSearchOff } from 'react-icons/md';
 
-interface IEmpty {}
-
-const Empty = ({}: IEmpty) => <div />;
+const Empty: React.FC = () => (
+  <div
+    className="container flex flex-col justify-center items-center text-neutral"
+    style={{ height: 'calc(100vh - 188px)' }}
+  >
+    <MdSearchOff size={64} />
+    <span>No asset found!</span>
+  </div>
+);
 
 export default Empty;
